94591bd43e4c2ce9d7528847ade2d773b690a245
DarioJZB <dariojzb87@gmail.com>
2024-09-05 19:40:38 -0400

    added files and completed Truck.ts

---
+++ src/classes/Truck.ts
@@ -0,0 +1,72 @@
+// import the Vehicle, Motorbike, Car, Wheel, and AbleToTow classes/interfaces
+import Vehicle from './Vehicle.js';
+import Motorbike from './Motorbike.js';
+import Car from './Car.js';
+import Wheel from './Wheel.js';
+import AbleToTow from '../interfaces/AbleToTow.js';
+
+class Truck extends Vehicle implements AbleToTow {
+  vin: string;
+  color: string;
+  make: string;
+  model: string;
+  year: number;
+  weight: number;
+  topSpeed: number;
+  wheels: Wheel[]; 
+  towingCapacity: number;
+
+  constructor(
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[],
+    towingCapacity: number,
+  ) {
+    super();
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+    this.wheels = wheels;
+    this.towingCapacity = towingCapacity;
+  }
+
+  tow (vehicle: Truck | Motorbike | Car): void {
+    if(vehicle){
+      if(vehicle.weight <= this.towingCapacity){
+        console.log(`The vehicle is being towed`);
+      } else {
+        console.log(`Vehicle is too heavy to be towed`);
+      }
+    } else {
+      console.log(`Vehicle does not exist`);
+    }
+  }
+
+  override printDetails(): void {
+    super.printDetails();
+
+    console.log(`VIN: ${this.vin}`);
+    console.log(`Make: ${this.make}`);
+    console.log(`Model: ${this.model}`);
+    console.log(`Year: ${this.year}`);
+    console.log(`Weight: ${this.weight}`);
+    console.log(`Top Speed: ${this.topSpeed}`);
+    console.log(`Color: ${this.color}`);
+    console.log(`Towing Capacity: ${this.towingCapacity}`);
+    for(let i = 0; i < this.wheels.length; i++){
+      console.log(`Wheel ${i + 1}: ${this.wheels[i].getDiameter} inch with a ${this.wheels[i].getTireBrand} tire`);
+    }
+  }
+}
+
+// Export the Truck class as the default export
+export default Truck;
